42db9c39509b15ca0cdbcc0564bdea159911ec15
zhaoyi27 <zhaoyi27@meituan.com>
2023-07-13 20:00:46 +0800

    chore: basic function determined & markdown sdk determined

---
+++ src/const/time-swiper.ts
@@ -0,0 +1,6 @@
+// export const TimeFilters = [
+//   {
+//     text: '今日',
+//     default: true,
+//   }
+// ]
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,11 @@
-import Header from './header'
+import ActionBar from './ActionBar'
 import Card from '@/components/Card'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Header />
+      <ActionBar />
       <Card />
     </>
   )
